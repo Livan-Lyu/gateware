bad7d2974fb7f1eb2d9592b5f964ec01a168ad3c
Lars Randers <lranders@mail.dk>
2025-09-14 11:25:30 +0200

    Complexity redux (2/5)
    - Collapse P8 port to a 44-bit vector
    
    Signed-off-by: Lars Randers <lranders@mail.dk>

--- sources/FPGA-design/script_support/components/CAPE/NONE/ADD_CAPE.tcl
+++ sources/FPGA-design/script_support/components/CAPE/NONE/ADD_CAPE.tcl
@@ -55,55 +55,24 @@ sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M
 sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[14:14]"} 
 sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[13:13]"} 
 sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[12:12]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[11:11]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[10:10]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[9:9]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[8:8]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[7:7]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[6:6]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[5:5]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[4:4]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[3:3]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[2:2]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[1:1]"} 
-sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[0:0]"} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[14:14]} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[12:12]} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[13:13]} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[22:22]} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[11:0]"} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[27:27]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[26:26]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[25:25]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[24:24]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[23:23]} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[15:15]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[22:22]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[21:21]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[20:20]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[19:19]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[18:18]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[17:17]} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[16:16]} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[6:6]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[0:0]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[1:1]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[2:2]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[3:3]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[4:4]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[5:5]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[7:7]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[8:8]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[9:9]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[10:10]} -port_name {} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[11:11]} -port_name {} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_0} -new_port_name {P8_3_USER_LED_0} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_1} -new_port_name {P8_4_USER_LED_1} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_2} -new_port_name {P8_5_USER_LED_2} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_3} -new_port_name {P8_6_USER_LED_3} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_4} -new_port_name {P8_7_USER_LED_4} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_5} -new_port_name {P8_8_USER_LED_5} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F} -new_port_name {P8_9_USER_LED_6} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_6} -new_port_name {P8_10_USER_LED_7} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_7} -new_port_name {P8_11_USER_LED_8} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_8} -new_port_name {P8_12_USER_LED_9} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_9} -new_port_name {P8_13_USER_LED_10} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_10} -new_port_name {P8_14_USER_LED_11} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[15:15]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[14:14]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[13:13]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[12:12]} 
+
+sd_connect_pins -sd_name ${sd_name} -pin_names {"P9[19]" "BVF_RISCV_SUBSYSTEM:I2C0_SCL"}
+sd_connect_pins -sd_name ${sd_name} -pin_names {"P9[20]" "BVF_RISCV_SUBSYSTEM:I2C0_SDA"}
+sd_connect_pins -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[11:0] P8}
